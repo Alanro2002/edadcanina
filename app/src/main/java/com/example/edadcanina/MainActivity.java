61f50186739c8038294ffa4418e83780858accd6
Alan Portillo <alanrodri02@hotmail.com>
2024-09-18 20:15:35 -0600

    Sale la edad del perro

--- app/src/main/java/com/example/edadcanina/MainActivity.java
+++ app/src/main/java/com/example/edadcanina/MainActivity.java
@@ -1,6 +1,10 @@
 package com.example.edadcanina;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,10 +19,23 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        EditText edadint = findViewById(R.id.age_id);
+        TextView resulttext = findViewById(R.id.result_text);
+        Button button = findViewById(R.id.boton);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String edad = edadint.getText().toString();
+                int edadint = Integer.parseInt(edad);
+                int resultado = edadint * 7;
+
+                String resulString = "Si fueras perro tu edad seria " + resultado + " años";
+                resulttext.setText(resulString);
+
+
+            }
         });
     }
 }
